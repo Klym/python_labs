e6a8ebc49fea575d80f5db5e17fa4a66894f5dba
Maksim Klimenko <klym.programmer@yandex.ru>
2016-06-01 17:39:30 +0300

    add reading -d key from console

--- лаб.3/task1.py
+++ лаб.3/task1.py
@@ -1,21 +1,28 @@
 # -*- coding: utf-8 -*-
-import struct, os
+import struct, os, getopt, sys
 
+opts, args = getopt.getopt(sys.argv[1:], "d", [])
+toDump = False
+
+for key, val in opts:
+    if key == "-d":
+        toDump = True
+        
 for root, dirs, files in os.walk(ur'D:\2015 - Younger Dreams'):
     for filename in files:
         with open(root + '\\' + filename, "rb") as f:
             size = os.path.getsize(root + '\\' + filename)
-            f.seek(size - 128)
-            structure = "3s30s30s30s4s28sbbb"
-            dump = f.read(struct.calcsize(structure))
-            tag, name, artist, album, year, comment, zero, track, genre = struct.unpack(structure, dump)
-            print name.decode('cp1251')
-            print artist.decode('cp1251')
-            print album.decode('cp1251')
+            sound = f.read(size - 128)
+            structure = "3s30s30s30s4s28sBBB"
+            buff = f.read(struct.calcsize(structure))
+            tag, name, artist, album, year, comment, zero, track, genre = struct.unpack(structure, buff)
+            print artist
+            print name
+            print album
             print year
-            print comment
-            print zero
-            print track
-            print genre
-            hexdump = ' '.join([hex(byte) for byte in struct.unpack("128b", dump)])
-            print(hexdump + "\n")
+            if toDump:
+                dump = [hex(byte) for byte in struct.unpack("128B", buff)]
+                dump = map(lambda h: h[-2:] if h[-2:][0] != 'x' else '0' + h[-1:], dump)
+                hexdump = ' '.join(dump)
+                print(hexdump)
+            print "\n"
